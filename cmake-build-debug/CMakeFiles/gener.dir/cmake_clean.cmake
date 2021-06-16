682a5bca2177b26a9f6efb0d5c6015828d4eeccf
TwistsOfFate <yu_fanqi@163.com>
2021-06-16 15:01:27 +0800

    Create repo

---
+++ cmake-build-debug/CMakeFiles/gener.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gener.dir/main.cpp.obj"
+  "gener.exe"
+  "gener.exe.manifest"
+  "gener.pdb"
+  "libgener.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
